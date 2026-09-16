37eb77c5a2327ac1c21608a0bb60b7559fd45165
faaazy <mikryukov811@mail.ru>
2026-09-17 00:49:51 +0400

    feat: implement festival status transitions

--- src/StageFlow.Domain/Festival/Festival.cs
+++ src/StageFlow.Domain/Festival/Festival.cs
@@ -2,19 +2,68 @@ namespace StageFlow.Domain.Festival;
 
 public class Festival
 {
-    public Guid Id{get; set;}
+    public Guid Id {get; set;}
 
-    public string Name{get; set;} = "";
+    public string Name {get; set;} = "";
 
-    public string? Description{get; set;} = "";
+    public string? Description {get; set;} = "";
 
-    public string Location{get; set;} = "";
+    public string Location {get; set;} = "";
 
-    public DateTime StartDate{get; set;}
+    public DateTime StartDate {get; set;}
 
-    public DateTime EndDate{get; set;}
+    public DateTime EndDate {get; set;}
 
-    public string TimeZone{get;set;} = "";
+    public string TimeZone {get;set;} = "";
 
-    public FestivalStatus Status{get; set;}
-}
+    public FestivalStatus Status {get; private set;}
+
+    public void ChangeStatus(FestivalStatus newStatus)
+    {
+        switch (Status)
+        {
+            case FestivalStatus.Draft:
+                if(newStatus == FestivalStatus.Published || 
+                    newStatus == FestivalStatus.Cancelled)
+                {
+                    Status = newStatus;
+                    return;
+                }
+                break;
+
+            case FestivalStatus.Published:
+                if(newStatus == FestivalStatus.OnSale ||
+                    newStatus == FestivalStatus.Cancelled)
+                {
+                    Status = newStatus;
+                    return;
+                }
+                break;
+
+            case FestivalStatus.OnSale:
+                if(newStatus == FestivalStatus.Live ||
+                    newStatus == FestivalStatus.Cancelled)
+                {
+                    Status = newStatus;
+                    return;
+                }
+                break;
+
+            case FestivalStatus.Live:
+                if(newStatus == FestivalStatus.Finished ||
+                    newStatus == FestivalStatus.Cancelled)
+                {
+                    Status = newStatus;
+                    return;
+                }
+                break;
+                
+            default:
+                throw new InvalidOperationException(
+                    $"Cannot change festival status from {Status} to {newStatus}.");
+        }
+        
+        throw new InvalidOperationException(
+                    $"Cannot change festival status from {Status} to {newStatus}.");
+    }
+};
